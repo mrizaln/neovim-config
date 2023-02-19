45bc1996aaaed832ec8be2d8f2eeb661aeb46226
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-02-19 21:04:16 +0700

    Update autocommands

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -6,3 +6,23 @@ vim.cmd([[
         \| exe "normal! g'\"" | endif
     endif
 ]])
+
+local enable_auto_format = false
+local exist, _ = pcall(require, "formatter")
+-- format after save
+if exist and enable_auto_format then
+	vim.cmd([[
+        augroup FormatAutogroup
+            autocmd!
+            autocmd BufWritePost * FormatWrite
+        augroup END
+    ]])
+end
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd([[
+    augroup highlight_yank
+        autocmd!
+            au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+    augroup END
+]])
