82a1eeeef8f3924134d4cec9c22ee6e23535b5b9
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-04-10 02:57:10 +0700

    Move and add new autocommands
    Formatter autocommands is moved to its plugin config rather that on
    global autocommands. Also added the capability to use autocommand on
    select filetype using rules. This rules can be overridden.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -7,22 +7,54 @@ vim.cmd([[
     endif
 ]])
 
-local enable_auto_format = false
-local exist, _ = pcall(require, "formatter")
--- format after save
-if exist and enable_auto_format then
-	vim.cmd([[
-        augroup FormatAutogroup
-            autocmd!
-            autocmd BufWritePost * FormatWrite
-        augroup END
-    ]])
-end
-
+--
 -- highlight yanked text for 200ms using the "Visual" highlight group
 vim.cmd([[
     augroup highlight_yank
         autocmd!
             au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
     augroup END
+]])
+
+--
+-- autosaves on rust file on file change (to trigger rust_analyzer)
+vim.cmd([[
+    augroup autosave_rust
+        autocmd!
+        autocmd FileType rust autocmd TextChanged,InsertLeave <buffer> if &readonly == 0 | silent write | endif
+    augroup END
+]])
+
+--
+-- set different tabstop size for md file
+vim.cmd([[
+    autocmd FileType markdown setlocal tabstop=2 softtabstop=2
+]])
+
+--
+-- turn on spell check on markdown files (for English and Indonesian)
+vim.cmd([[
+    autocmd FileType markdown setlocal spelllang=en_us,id spell
+]])
+
+--
+-- keep cursor at `scrolloff` distance from end of window
+vim.cmd([[
+augroup KeepFromBottom
+    autocmd!
+    autocmd CursorMoved * call AvoidBottom()
+    autocmd TextChangedI * call AvoidBottom()
+augroup END
+
+function AvoidBottom()
+    let distance_from_end = winheight(0) - winline()
+    let lines_below_scrolloff = distance_from_end - &scrolloff
+
+    " if cursor is below scrolloff distance from bottom of window, scroll buffer up that number of lines
+    if lines_below_scrolloff < 0
+        normal! 5<C-E>
+        let n = -lines_below_scrolloff
+        execute 'normal! ' . n . "\<C-E>"
+    endif
+endfunction
 ]])
--- .config/nvim/lua/config/plugin/formatter_nvim.lua
+++ .config/nvim/lua/config/plugin/formatter_nvim.lua
@@ -6,31 +6,57 @@ local util = require("formatter.util")
 -- use project specific format if [.clang-format] of [_clang-format] is available
 -- else use [$HOME/.config/nvim/_clang-format]
 local clangformat = function()
+	local style_file = nil
+
+	local fileExist = _G.myUtils.fileExist
+
 	local workspaces = vim.lsp.buf.list_workspace_folders()
 	if #workspaces >= 1 then
-		local fileExist = _G.myUtils.fileExist
-		if fileExist(workspaces[1] .. "/.clang-format") or fileExist(workspaces[1] .. "/_clang-format") then
-			return require("formatter.defaults.clangformat")
+		if fileExist(workspaces[1] .. "/_clang-format") then
+			style_file = workspaces[1] .. "/_clang-format"
+		elseif fileExist(workspaces[1] .. "/.clang-format") then
+			style_file = workspaces[1] .. "/.clang-format"
 		end
 	end
 
-	local style_file
-	style_file = os.getenv("HOME") .. "/.config/nvim/_clang-format"
+	-- single file mode usually returns 0 #workspaces
+	if style_file == nil then
+		if fileExist(os.getenv("HOME") .. "/.config/nvim/_clang-format") then
+			style_file = os.getenv("HOME") .. "/.config/nvim/_clang-format"
+		elseif fileExist(os.getenv("HOME") .. "/.config/nvim/.clang-format") then
+			style_file = os.getenv("HOME") .. "/.config/nvim/.clang-format"
+		end
+	end
 
-	return {
-		exe = "clang-format",
-		args = {
-			"-assume-filename",
-			util.escape_path(util.get_current_buffer_file_name()),
-			util.escape_path("-style=file:" .. style_file),
-		},
-		stdin = true,
-		try_node_modules = true,
-	}
+	if style_file == nil then
+		print("No clang_format found, use defaults")
+		print("Usually here: " .. os.getenv("HOME") .. "/.config/nvim/_clang-format")
+		return (require("formatter.defaults.clangformat"))()
+	else
+		return {
+			exe = "clang-format",
+			args = {
+				"-assume-filename",
+				util.escape_path(util.get_current_buffer_file_name()),
+				util.escape_path("-style=file:" .. style_file),
+			},
+			stdin = true,
+			try_node_modules = true,
+		}
+	end
 end
 
--- eslint_d
-local eslint_d = require("formatter.defaults.eslint_d")
+-- local eslint_d = require("formatter.defaults.eslint_d")
+
+local rustfmt = require("formatter.filetypes.rust").rustfmt
+
+local prettier = function()
+	local _prettier = require("formatter.filetypes.markdown").prettier()
+
+	-- use tab-width value the same as nvim's configured tabstop value
+	table.insert(_prettier.args, "--tab-width=" .. vim.opt.tabstop._value)
+	return _prettier
+end
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup({
@@ -79,6 +105,10 @@ require("formatter").setup({
 
 		python = require("formatter.filetypes.python").black,
 
+		rust = rustfmt,
+
+		markdown = prettier,
+
 		-- Use the special "*" filetype for defining formatter configurations on
 		-- any filetype
 		["*"] = {
@@ -89,5 +119,111 @@ require("formatter").setup({
 	},
 })
 
+--
+--------------------------------------------------------------------------------
+--[ autocommand ]--
+--
+
+-- auto format rule
+vim.cmd([[
+    let g:formatter_auto_format_rules = {
+        \'c' : 1,
+        \'cpp' : 1,
+        \'lua' : 1,
+        \'markdown' : 1,
+        \'python' : 0,
+        \'other' : 0,
+    \}
+]])
+
+-- auto format get filetype
+vim.cmd([[
+    function FormatterAutoCommandGetFiletype()
+        let l:filetype = &filetype
+
+        if empty(l:filetype)
+            let l:filetype = "other"
+        endif
+
+        if !has_key(g:formatter_auto_format_rules, l:filetype)
+            return "other"
+        else
+            return l:filetype
+        endif
+    endfunction
+
+]])
+
+-- auto format init
+vim.cmd([[
+    let g:formatter_auto_format_enabled = {}
+    let g:formatter_auto_format_overridden = {}
+
+    function FormatterAutoCommandInit()
+        let l:filetype = FormatterAutoCommandGetFiletype()
+
+        if has_key(g:formatter_auto_format_overridden, l:filetype)
+            return
+        endif
+
+        let l:rule = g:formatter_auto_format_rules[l:filetype]
+        let g:formatter_auto_format_enabled[l:filetype] = l:rule
+    endfunction
+]])
+
+-- auto format status
+vim.cmd([[
+    function FormatterAutoCommandStatus()
+        let l:filetype = FormatterAutoCommandGetFiletype()
+
+        return g:formatter_auto_format_enabled[l:filetype]
+    endfunction
+]])
+
+-- auto format command
+vim.cmd([[
+    function FormatterAutoCommandFunction(state)
+        let l:filetype = FormatterAutoCommandGetFiletype()
+
+        let g:formatter_auto_format_overridden[l:filetype] = 1
+
+        if empty(a:state)
+            let g:formatter_auto_format_enabled[l:filetype] = (g:formatter_auto_format_enabled[l:filetype] + 1) % 2 " toggle
+        else
+            let g:formatter_auto_format_enabled[l:filetype] = a:state
+        endif
+    endfunction
+
+
+    command -nargs=0 FormatWriteAutoCmdStatus echo "FormatterAutoCommandStatus (" . &filetype . "): " . FormatterAutoCommandStatus()
+    command -nargs=? FormatWriteAutoCmd call FormatterAutoCommandFunction("<args>") | FormatWriteAutoCmdStatus
+    command -nargs=0 FormatWriteAutoCmdToggle call FormatterAutoCommandFunction("") | FormatWriteAutoCmdStatus
+    command -nargs=0 FormatWriteAutoCmdEnable call FormatterAutoCommandFunction(1) | FormatWriteAutoCmdStatus
+    command -nargs=0 FormatWriteAutoCmdDisable call FormatterAutoCommandFunction(0) | FormatWriteAutoCmdStatus
+]])
+
+-- format after save autocommand
+vim.cmd([[
+    augroup FormatAutogroup
+        autocmd!
+        autocmd BufEnter * call FormatterAutoCommandInit()
+        autocmd BufWritePost * call MyFormatWrite()
+    augroup END
+
+    function MyFormatWrite()
+        let l:filetype = FormatterAutoCommandGetFiletype()
+
+        if g:formatter_auto_format_enabled[l:filetype] == 1
+           FormatWrite
+        endif
+    endfunction
+]])
+
+--
+--------------------------------------------------------------------------------
+--[ mapping ]--
+
 vim.cmd([[nnoremap <silent> <leader>ff :Format<CR>]])
 vim.cmd([[nnoremap <silent> <leader>fw :FormatWrite<CR>]])
+vim.cmd([[nnoremap <silent> <leader>ft :FormatWriteAutoCmdToggle<CR>]])
+vim.cmd([[nnoremap <silent> <leader>fs :FormatWriteAutoCmdStatus<CR>]])
